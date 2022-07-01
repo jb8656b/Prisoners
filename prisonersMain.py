894a4c54c1c2bd68a9313c7b8bfa6b23ab0c73c9
James Blair <jb8656b@student.american.edu>
2022-07-01 13:39:40 -0400

    starting with dictionaries

--- prisonersMain.py
+++ prisonersMain.py
@@ -2,13 +2,18 @@ import random
 def prisoners(numOfPrisoners):
     arrayOfPrisoners = []
     arrayOfBoxes = []
+    arrayOfNumbers = []
+    boxes = {}
     for i in range(numOfPrisoners):
         arrayOfPrisoners.append(i)
-    print(arrayOfPrisoners)
     for i in range(numOfPrisoners):
         arrayOfBoxes.append(i)
-    random.shuffle(arrayOfBoxes)
-    print(arrayOfBoxes)
+    for i in range(numOfPrisoners):
+        arrayOfNumbers.append(i)
+    random.shuffle(arrayOfNumbers)
+    print("Prisoners:", arrayOfPrisoners)
+    print("Boxes:",arrayOfBoxes)
+    # for i in prisoners:
     return True
 
 prisoners(100)
