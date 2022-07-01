cbfdd517f7111da34f9f98f021fcc34c835814e8
James Blair <jb8656b@student.american.edu>
2022-07-01 16:23:00 -0400

    Completed in Python

--- prisonersMain.py
+++ prisonersMain.py
@@ -1,42 +1,48 @@
 import random
 def prisoners(numOfPrisoners):
+    # This function returns True if the prisoners
+    # succeed, returns false if the prisoners fail
     arrayOfPrisoners = []
-    arrayOfBoxes = []
     arrayOfNumbers = []
     boxes = {}
     for i in range(numOfPrisoners):
         arrayOfPrisoners.append(i)
-    # for i in range(numOfPrisoners):
-    #     arrayOfBoxes.append(i)
     for i in range(numOfPrisoners):
         arrayOfNumbers.append(i)
     random.shuffle(arrayOfNumbers)
-    print("Prisoners:", arrayOfPrisoners)
-    # print("Boxes:",arrayOfBoxes)
-    print("Numbers:", arrayOfNumbers)
+    # print("Prisoners:", arrayOfPrisoners)
+    # print("Numbers:", arrayOfNumbers)
     for i in range(len(arrayOfNumbers)):
         boxes[i] = arrayOfNumbers[i]
-    print("Boxes dictionary:", boxes)
+    # print("Boxes dictionary:", boxes)
     countOfBoxesChecked = 0
     boxCountCheckLimit = numOfPrisoners / 2
     for prisonerNumber in arrayOfPrisoners:
-        print("Prisoner", prisonerNumber)
-        print(boxes[prisonerNumber])
+        # print("Prisoner", prisonerNumber)
         currentBox = prisonerNumber
-        while countOfBoxesChecked < boxCountCheckLimit:
-            print("Check number", countOfBoxesChecked)
-            print("Prisoner Number is", prisonerNumber, ". Box Number is", currentBox)
+        while countOfBoxesChecked <= boxCountCheckLimit:
+            # print("Check number", countOfBoxesChecked)
+            # print("Prisoner Number is", prisonerNumber, ". Box Number is", currentBox)
             if countOfBoxesChecked >= boxCountCheckLimit:
-                print("Prisoners Failed")
+                # print("Prisoners Failed")
                 return False
             if boxes[currentBox] == prisonerNumber:
-                print("Box has been found")
-                countOfBoxesChecked = 0
+                # print("Box has been found")
                 break
             else:
                 currentBox = boxes[currentBox]
                 countOfBoxesChecked += 1
-    print("Prisoners Succeeded")
+        countOfBoxesChecked = 0
+    # print("Prisoners Succeeded")
     return True
 
-prisoners(100)
+successes = 0
+fails = 0
+for i in range(100000):
+    value = prisoners(100)
+    if value == True:
+        successes += 1
+    if value == False:
+        fails += 1
+print("Successes:", successes)
+print("Fails:", fails)
