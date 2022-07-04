0689ed49dd88ba82bdb2099a9cd3269f2dd889dc
James Blair <jb8656b@student.american.edu>
2022-07-04 13:58:38 -0400

    started java version

--- Prisoners.java
+++ Prisoners.java
@@ -1,5 +1,14 @@
+import java.util.*;
 public class Prisoners {
     public static void main(String[] args){
         System.out.println("Test");
+        List<Box> boxes = new ArrayList<Box>();
+        System.out.println(boxes);
+    }
+    public static class Prisoner{
+        int prisonerNumber;
+    }
+    public static class Box{
+        private int hiddenNumber;
     }
 }
