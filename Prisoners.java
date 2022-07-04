21ea811bba9bac437f73c970ad65a363f5738070
James Blair <jb8656b@student.american.edu>
2022-07-04 14:23:18 -0400

    creating one box, lol

--- Prisoners.java
+++ Prisoners.java
@@ -4,11 +4,17 @@ public class Prisoners {
         System.out.println("Test");
         List<Box> boxes = new ArrayList<Box>();
         System.out.println(boxes);
+        Box testBox = new Box();
+        System.out.println(testBox);
+        // boxes.add(new Box)
     }
     public static class Prisoner{
         int prisonerNumber;
     }
     public static class Box{
         private int hiddenNumber;
+        // public Box(int hiddenNumber){
+        //     hiddenNumber = hiddenNumber;
+        // }
     }
 }
