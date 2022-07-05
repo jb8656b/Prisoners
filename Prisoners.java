966c3e1a0f8e6e12f744fa077f8d9939601dff96
James Blair <jb8656b@student.american.edu>
2022-07-04 20:00:40 -0400

    moved main stuff to its own function

--- Prisoners.java
+++ Prisoners.java
@@ -1,14 +1,7 @@
 import java.util.*;
 public class Prisoners {
     public static void main(String[] args){
-        System.out.println("Test");
-        List<Box> boxes = new ArrayList<Box>();
-        System.out.println(boxes);
-        Box testBox = new Box(3);
-        System.out.println(testBox.getHiddenNumber());
-        Prisoner testPrisoner = new Prisoner(2);
-        System.out.println(testPrisoner.getPrisonerNumber());
-        // boxes.add(new Box)
+        System.out.println(prisonExperiment());
     }
     public static class Prisoner{
         private int prisonerNumber;
@@ -20,7 +13,7 @@ public class Prisoners {
             return this.prisonerNumber;
         }
     }
-    
+
     public static class Box{
         private int hiddenNumber;
         public Box(int hiddenNumberHolder){
@@ -29,5 +22,16 @@ public class Prisoners {
         public int getHiddenNumber(){
             return this.hiddenNumber;
         }
+    }
+    static boolean prisonExperiment(){
+        System.out.println("Test");
+        List<Box> boxes = new ArrayList<Box>();
+        List<Prisoner> prisoners = new ArrayList<Prisoner>();
+        Box testBox = new Box(3);
+        System.out.println(testBox.getHiddenNumber());
+        Prisoner testPrisoner = new Prisoner(2);
+        System.out.println(testPrisoner.getPrisonerNumber());
+        boxes.add(testBox);
+        return true;
     }
 }
